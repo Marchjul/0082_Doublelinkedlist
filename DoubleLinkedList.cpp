b8f3a2ca8846168afb63f2a1b7fa1b7c97937e6c
Marchjul <marchellogiovani28@gmail.com>
2026-04-30 22:11:15 +0700

    if

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -186,4 +186,17 @@ class DoubleLinkedList
     }
 
     void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+    }
 };
